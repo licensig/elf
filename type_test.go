c72da63d4ac838b5b5a7962a2542a1882e72b0ef
Tahir Murata <147841049+tahirmurata@users.noreply.github.com>
2024-12-02 22:21:56 +0900

    Write test for StringToInts

---
+++ type_test.go
@@ -0,0 +1,35 @@
+package elf_test
+
+import (
+	"fmt"
+	"slices"
+	"testing"
+
+	"github.com/tahirmurata/elf"
+)
+
+func TestStringToInts(t *testing.T) {
+	tests := []struct {
+		str, sep string
+		want     []int
+	}{
+		{"12 345 678 910", " ", []int{12, 345, 678, 910}},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%s,%s", tt.str, tt.sep)
+		t.Run(testname, func(t *testing.T) {
+			ans := elf.StringToInts(tt.str, tt.sep)
+			// if ans != tt.want {
+			if slices.Compare(ans, tt.want) != 0 {
+				t.Errorf("got %d, want %d", ans, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkStringToInts(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		elf.StringToInts("12 345 678 910", " ")
+	}
+}
